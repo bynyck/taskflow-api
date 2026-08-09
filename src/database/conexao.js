ccd9a600c35f7917f35b69b3ee531240054fbce2
nykthedev <nicollasnascimento.pc@gmail.com>
2026-08-09 01:30:23 -0300

    chore: prepara aplicação para deploy

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -2,12 +2,18 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const configuracaoBanco = process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL
+    }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+
+const pool = new Pool(configuracaoBanco);
 
 export { pool };
